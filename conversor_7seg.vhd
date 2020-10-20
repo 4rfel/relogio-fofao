aa8d8f9d7ca483eb4c534d71be1c0cf460e82598
joaopmjm <joaopmjm@al.insper.edu.br>
2020-10-20 13:05:59 -0300

    Conversor atualizado, mais simples, sem tantas funcoes, mas nao testado

--- conversor_7seg.vhd
+++ conversor_7seg.vhd
@@ -18,63 +18,48 @@ entity conversor_7seg is
 end entity;
 
 architecture comportamento of conversor_7seg is
-    SIGNAL hex           : std_logic_vector(13 downto 0);
+    SIGNAL hexa   : std_logic_vector(13 downto 0);
     SIGNAL input : unsigned(7 downto 0);
     
-    FUNCTION Conv7Seg(
-        digit : std_logic_vector(3 downto 0)
+    function converte(
+        digito : std_logic_vector(3 downto 0)
     ) return std_logic_vector
     is begin
-        case digit is
-            when x"0" => return "1000000";
-            when x"1" => return "1111001";
-            when x"2" => return "0100100";
-            when x"3" => return "0110000";
-            when x"4" => return "0011001";
-            when x"5" => return "0010010";
-            when x"6" => return "0000010";
-            when x"7" => return "1111000";
-            when x"8" => return "0000000";
-            when x"9" => return "0010000";
-                when others => return "1111111";
-        end case;
+        if digito is x"0" return "1000000";
+        if digito is x"1" return "1111001";
+        if digito is x"2" return "0100100";
+        if digito is x"3" return "0110000";
+        if digito is x"4" return "0011001";
+        if digito is x"5" return "0010010";
+        if digito is x"6" return "0000010";
+        if digito is x"7" return "1111000";
+        if digito is x"8" return "0000000";
+        if digito is x"9" return "0010000";
+        return "1111111";
     end;
 
-    FUNCTION decimal7Segmentos(
-        value: unsigned;
-        digitos: integer
-    ) return std_logic_vector
-    is 
-        variable LEDS : std_logic_vector(digitos*7-1 downto 0);
-        variable quo : unsigned(7 downto 0);
-        variable remainder: unsigned(3 downto 0);
-    begin
-        quo := value;
-        FOR i in 0 to digitos-1 loop
-            restante := resize(quo mod 10, 4);
-            quo  := quo/10;
-            LEDS(i*7+6 downto i*7) := Conv7Seg(std_logic_vector(restante)
-            );
-        end loop;
-        return LEDS;
-    end;
-
-
+variable LEDS : std_logic_vector(digitos*7-1 downto 0);
+variable quociente : unsigned(7 downto 0);
+variable resto: unsigned(3 downto 0);
 begin
     input <= unsigned(dadoHex);
-    hex <= decimal7Segmentos(
-        input,
-        2
-    );
+    quociente := input;
+    resto := resize(quociente mod 10, 4);
+    quociente := quociente/10;
+    LEDS(6 downto 0) := converte(std_logic_vector(resto));
+
+    resto := resize(quociente mod 10, 4);
+    quociente := quociente/10;
+    LEDS(15 downto 7) := converte(std_logic_vector(resto));
 
     saida7seg1 <= "1100010" when (overFlow = '1') else
         "1111111" when (apaga = '1' and negativo = '0') else
         "0111111" when (apaga = '0' and negativo = '1') else
-        hex(13 downto 7);
+        hexa(13 downto 7);
 
     
     saida7seg2 <= "1100010" when (overFlow = '1') else
         "1111111" when (apaga = '1' and negativo = '0') else
         "0111111" when (apaga = '0' and negativo = '1') else
-        hex(6 downto 0);
+        hexa(6 downto 0);
 end architecture;
